1857dad8f42e244b4ebc2a963c1282cbedce6087
Ryan Case <Ryan_Case@outlook.com>
2019-01-09 22:01:25 -0500

    Updates

---
+++ precache-manifest.8db2937f938e188993e448fd46a4fe2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e14a6b9301c54bec253",
+    "url": "/Ryan-T-Case/react-memory-game.git/static/js/runtime~main.2e14a6b9.js"
+  },
+  {
+    "revision": "820b76766604d2d45420",
+    "url": "/Ryan-T-Case/react-memory-game.git/static/js/main.820b7676.chunk.js"
+  },
+  {
+    "revision": "9b44338201aa54907bf9",
+    "url": "/Ryan-T-Case/react-memory-game.git/static/js/1.9b443382.chunk.js"
+  },
+  {
+    "revision": "820b76766604d2d45420",
+    "url": "/Ryan-T-Case/react-memory-game.git/static/css/main.5dfab7e1.chunk.css"
+  },
+  {
+    "revision": "9e9ff56cb8db55a0f2af3e7fc43c6725",
+    "url": "/Ryan-T-Case/react-memory-game.git/index.html"
+  }
+];
